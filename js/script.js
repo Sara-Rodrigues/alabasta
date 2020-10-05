3f18c0ee2631013e323142c3eeca88acc647f153
Sara-Rodrigues <sara.slcr67@gmail.com>
2020-10-05 17:57:07 -0300

    arquivos js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+new SimpleSlide({
+    slide: "slideshow",
+    nav: true, // se deve ou não mostrar a navegação
+    time: 3000, // tempo de transição dos slides
+    pauseOnHover: true // pausa a transição automática
+  });
